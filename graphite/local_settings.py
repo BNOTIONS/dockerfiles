3634f52ac72757f3b95c4abf6d1076c9a183d407
Jonathon W. Marshall <jonathon.marshall@gmail.com>
2014-02-24 14:45:53 -0500

    simplify graphite supervisor job name

--- graphite/local_settings.py
+++ graphite/local_settings.py
@@ -1,1 +1,10 @@
+from os import environ
+
 TIME_ZONE = 'UTC'
+
+# Set SECRET_KEY if passed in via environment variable
+try: 
+    if environ['SECRET_KEY']:
+        SECRET_KEY = environ['SECRET_KEY']
+except KeyError:
+    pass
