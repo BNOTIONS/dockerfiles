6be4aec7d3317f6bcabb26f0693c94c568c760a5
Jonathon W. Marshall <jonathon@bnotions.com>
2014-02-28 12:15:17 -0500

    persist SECRET_KEY in supervisor config

--- graphite/run.sh
+++ graphite/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ ! -z $SECRET_KEY ]; then
+  sed -E -i "s/^environment = (.*)$/environment = \1,SECRET_KEY='$SECRET_KEY'/" /etc/supervisor/conf.d/supervisord.conf
+fi
+
 mkdir -p /opt/graphite/storage/whisper
 touch /opt/graphite/storage/graphite.db /opt/graphite/storage/index
 chown -R www-data /opt/graphite/storage
